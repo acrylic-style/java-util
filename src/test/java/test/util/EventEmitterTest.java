3ad099e7d9089e0d1004d7e75d8ba1aaa4424ab4
acrylic-style <me@acrylicstyle.xyz>
2020-04-08 09:37:42 +0900

    remove test messages

--- src/test/java/test/util/EventEmitterTest.java
+++ src/test/java/test/util/EventEmitterTest.java
@@ -7,8 +7,7 @@ public class EventEmitterTest extends EventEmitter {
     @Test
     public void onAndEmit() {
         this.on("test_", (o) -> {
-            String a = (String) o[0];
-            System.out.println("Received input: " + a);
+            /* do something */
         });
         this.emit("test_", "Hello o/ on");
         assert this.listeners("test_").size() == 1 : "Expected 1 listener count, but got " + this.listeners("test_").size();
@@ -17,8 +16,7 @@ public class EventEmitterTest extends EventEmitter {
     @Test
     public void onceAndEmit() {
         this.once("test_once", (o) -> {
-            String a = (String) o[0];
-            System.out.println("Received input: " + a);
+            /* do something */
         });
         this.emit("test_once", "Hello o/ once");
         assert this.listeners("test_once").size() == 0 : "Expected 0 listener count, but got " + this.listeners("test_test").size();
--- src/test/java/test/util/PromiseTest.java
+++ src/test/java/test/util/PromiseTest.java
@@ -2,9 +2,7 @@ package test.util;
 
 import org.junit.Test;
 import util.CollectionList;
-import util.ICollectionList;
 import util.promise.Promise;
-import util.promise.UnhandledPromiseException;
 
 import java.util.Objects;
 
@@ -15,14 +13,14 @@ public class PromiseTest {
     public void promiseTest() {
         Promise<String> promise = async(o -> "It works! (Data: " + o + ") (PromiseTest#promiseTest)");
         String string = (String) await(promise, "Test");
-        System.out.println(string);
+        assert string != null;
+        assert string.equals("It works! (Data: Test) (PromiseTest#promiseTest)");
     }
 
     @Test
     public void promiseHeavyTask() {
         Promise<String> promise = async(o -> {
             try {
-                System.out.println("Gonna sleep. Don't disturb me.");
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -30,7 +28,8 @@ public class PromiseTest {
             return "It works! (Data: " + o + ") (PromiseTest#promiseHeavyTask)";
         });
         String string = (String) await(promise, "Test");
-        System.out.println(string);
+        assert string != null;
+        assert string.equals("It works! (Data: Test) (PromiseTest#promiseHeavyTask)");
     }
 
     @Test
@@ -79,51 +78,6 @@ public class PromiseTest {
         CollectionList<String> result = Objects.requireNonNull(awaitT(Promise.all(promise, promise2, promise3, promise4, promise5))).map(o -> (String) o);
         long end = System.currentTimeMillis();
         if (end-start > 1500) throw new AssertionError("Took " + (end - start) + " to complete (expected <= 1500ms)");
-        System.out.println(result.join(", "));
         assert result.size() == 5;
-    }
-
-    @Test
-    public void promiseThen() {
-        Promise<String> promise = async(o -> "J,u,s,t,," + o);
-        String[] string = promise.then(o -> {
-            String s = (String) o;
-            return s.split(",");
-        }).complete("B,u,r,n,,C,o,o,k,i,e");
-        System.out.println(ICollectionList.asList(string).join(""));
-    }
-
-    @Test
-    public void promiseThrowable() {
-        Promise<String> promise = async(o -> {
-            throw new RuntimeException("??? (Test exception, please ignore this)");
-        });
-        String s = (String) await(promise.catch_(e -> {
-            System.out.println("An error occurred inside the Promise: " + e);
-            Throwable throwable = (Throwable) e;
-            throwable.printStackTrace();
-            return null;
-        }), "no");
-        System.out.println("s: " + s);
-    }
-
-    @Test(expected = UnhandledPromiseException.class)
-    public void promiseUnhandledThrowable() {
-        Promise<String> promise = async(o -> {
-            throw new RuntimeException("???");
-        });
-        String s = (String) await(promise, "no");
-        System.out.println("s: " + s);
-    }
-
-    @Test
-    public void queue() {
-        new Promise<Object>() {
-            @Override
-            public Object apply(Object o) {
-                System.out.println("AAAAA");
-                return null;
-            }
-        }.queue();
     }
 }
