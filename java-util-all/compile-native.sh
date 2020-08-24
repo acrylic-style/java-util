13933026d918f5072fa776dce375494d3f7d6111
acrylic-style <me@acrylicstyle.xyz>
2020-08-24 17:09:55 +0900

    compile in linux

--- java-util-all/compile-native.sh
+++ java-util-all/compile-native.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# Waterfall - rewrite below to extend platform support
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # brew install mbedtls zlib
   PREFIX="osx-"
   CXX_ARGS="/usr/local/lib/libmbedcrypto.a -lz -I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/ -I/usr/local/include -L/usr/local/lib"
+elif [[ "$OSTYPE" == "cygwin"* ]]; then
+  PREFIX="windows-"
+  CXX_ARGS="-lz -I$JAVA_HOME/include/ -I$JAVA_HOME/include/win32/"
 else
   # apt-get install libmbedtls-dev zlib1g-dev
-  CXX_ARGS="-lz -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/ -I$JAVA_HOME/include/win32/"
+  CXX_ARGS="-lz -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
 fi
 
 CXX="gcc -shared -fPIC -O3 -Wall -Werror"
