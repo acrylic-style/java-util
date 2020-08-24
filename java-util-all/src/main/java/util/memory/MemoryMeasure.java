10b86954e108999d6895d9aeb0caaf448e7bf0c9
acrylic-style <me@acrylicstyle.xyz>
2020-08-24 17:29:11 +0900

    this is reliable way

--- java-util-all/src/main/java/util/memory/MemoryMeasure.java
+++ java-util-all/src/main/java/util/memory/MemoryMeasure.java
@@ -4,6 +4,11 @@ import com.google.common.annotations.Beta;
 import org.jetbrains.annotations.NotNull;
 import util.jni.NativeCode;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 @Beta
 public class MemoryMeasure {
     private static final NativeCode NATIVE_CODE = new NativeCode("native-memory-measure");
@@ -21,4 +26,14 @@ public class MemoryMeasure {
 
     @Beta
     public native long sizeof(Object o);
+
+    public static long getSize(Object o) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(baos);
+            out.writeObject(o);
+            out.close();
+            return baos.toByteArray().length;
+        } catch (IOException e) { throw new RuntimeException(e); }
+    }
 }
