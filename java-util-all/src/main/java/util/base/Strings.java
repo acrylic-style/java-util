a49e2e01f27f9db5737a11d5bae1253d74d4175a
acrylic-style <ilyy@outlook.jp>
2021-03-17 21:55:10 +0900

    fixed levenshtein distance

--- java-util-all/src/main/java/util/base/Strings.java
+++ java-util-all/src/main/java/util/base/Strings.java
@@ -1,7 +1,6 @@
 package util.base;
 
 import org.jetbrains.annotations.NotNull;
-import util.CollectionList;
 import util.MathUtils;
 import util.Validate;
 
@@ -14,32 +13,44 @@ public class Strings {
         return result.toString();
     }
 
-    /* // Broken thing
     public static int levenshteinDistance(@NotNull String a, @NotNull String b) {
+        return levenshteinDistance(a, b, Integer.MAX_VALUE);
+    }
+
+    public static int levenshteinDistance(@NotNull String a, @NotNull String b, int limit) {
         if (a.length() == 0) return b.length();
         if (b.length() == 0) return a.length();
-        CollectionList<Integer> row = new CollectionList<>();
-        for (int i = 0; i < a.length(); i++) row.add(i);
-        int prev;
-        for (int i = 0; i < b.length(); i++) {
-            prev = i;
-            for (int j = 0; j < a.length(); j++) {
-                int val;
-                if (charAt(b, i - 1).equals(charAt(a, j - 1))) {
-                    val = row.get(Math.max(j - 1, 0));
-                } else {
-                    val = MathUtils.min((j - 1 < 0 ? null : row.get(j - 1) + 1),
-                            prev + 1,
-                            row.get(j) + 1);
-                }
-                row.set(Math.max(j - 1, 0), prev);
-                prev = val;
-            }
-            row.set(a.length(), prev);
+        if (a.equals(b)) return 0;
+        int[] a1 = new int[b.length() + 1];
+        int[] a2 = new int[b.length() + 1];
+        int[] temp;
+        for (int i = 0; i < a1.length; i++) {
+            a1[i] = i;
         }
-        return row.get(a.length());
+        for (int i = 0; i < a.length(); i++) {
+            a2[0] = i + 1;
+            int min = a2[0];
+            for (int i1 = 0; i1 < b.length(); i1++) {
+                int cost = 1;
+                if (a.charAt(i) == b.charAt(i1)) {
+                    cost = 0;
+                }
+                a2[i1 + 1] = MathUtils.min(
+                        a2[i1] + 1,
+                        a1[i1 + 1] + 1,
+                        a1[i1] + cost
+                );
+                min = Math.min(min, a2[i1 + 1]);
+            }
+            if (min >= limit) {
+                return limit;
+            }
+            temp = a1;
+            a1 = a2;
+            a2 = temp;
+        }
+        return a1[b.length()];
     }
-    */
 
     @NotNull
     public static String charAt(@NotNull String s, int index) {
