ef91e52d7e2340662c79cbcd84704a51d2cb034f
acrylic-style <ilyy@outlook.jp>
2021-03-18 10:49:54 +0900

    use value instead of AtomicReference

--- java-util-all/src/main/java/util/ref/AtomicReferencePredicateUpdater.java
+++ java-util-all/src/main/java/util/ref/AtomicReferencePredicateUpdater.java
@@ -7,12 +7,11 @@ import util.Validate;
 import util.magic.Magic;
 
 import java.io.Serializable;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiPredicate;
 
 public class AtomicReferencePredicateUpdater<V> implements Serializable {
     private static final long serialVersionUID = Magic.VERSION;
-    private final AtomicReference<V> reference;
+    private V value;
     private final BiPredicate<V, V> setterPredicate;
     @NotNull
     private String setterRejectedMessage = "";
@@ -26,7 +25,7 @@ public class AtomicReferencePredicateUpdater<V> implements Serializable {
     public AtomicReferencePredicateUpdater(@NotNull BiPredicate<V, V> setterPredicate, @Nullable V initialValue) {
         Validate.notNull(setterPredicate, "predicate cannot be null");
         this.setterPredicate = setterPredicate;
-        this.reference = new AtomicReference<>(initialValue);
+        this.value = initialValue;
     }
 
     /**
@@ -37,7 +36,7 @@ public class AtomicReferencePredicateUpdater<V> implements Serializable {
     public AtomicReferencePredicateUpdater(@NotNull BiPredicate<V, V> setterPredicate) {
         Validate.notNull(setterPredicate, "predicate cannot be null");
         this.setterPredicate = setterPredicate;
-        this.reference = new AtomicReference<>();
+        this.value = null;
     }
 
     @NotNull
@@ -51,7 +50,7 @@ public class AtomicReferencePredicateUpdater<V> implements Serializable {
         if (!setterPredicate.test(get(), newValue)) {
             throw new RejectedFieldOperationException("Field set '" + get() + "' to '" + newValue + "' was rejected" + getSetterRejectedMessage());
         }
-        this.reference.set(newValue);
+        this.value = newValue;
     }
 
     @NotNull
@@ -61,7 +60,9 @@ public class AtomicReferencePredicateUpdater<V> implements Serializable {
         return " (" + setterRejectedMessage + ")";
     }
 
-    public V get() { return reference.get(); }
+    public V get() {
+        return value;
+    }
 
     public boolean is(V value) { return value == null ? get() == null : value.equals(get()); }
 
