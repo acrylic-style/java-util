1ad27899d8a5b5342ef4b5de4aff845df3085ee4
acrylic-style <me@acrylicstyle.xyz>
2022-12-12 01:23:13 +0900

    Fix jvm crash when using NativeUtil
    workaround

--- reflector/src/main/java/xyz/acrylicstyle/util/reflector/executor/MethodExecutorNativeUtil.java
+++ reflector/src/main/java/xyz/acrylicstyle/util/reflector/executor/MethodExecutorNativeUtil.java
@@ -43,18 +43,18 @@ public class MethodExecutorNativeUtil implements MethodExecutor {
     }
 
     @Override
-    public Object invoke(@NotNull Method method, Object instance, Object @NotNull ... args) {
-        return NativeUtil.invoke(method, instance, args);
+    public Object invoke(@NotNull Method method, Object instance, Object... args) {
+        return NativeUtil.invoke(method, instance, args == null ? new Object[0] : args);
     }
 
     @Override
     public Object invokeSpecial(@NotNull Method method, @NotNull Object instance, Object... args) {
-        return NativeUtil.invokeNonvirtual(method, instance, args);
+        return NativeUtil.invokeNonvirtual(method, instance, args == null ? new Object[0] : args);
     }
 
     @Override
     public <T> @NotNull T newInstance(@NotNull Constructor<T> constructor, Object... args) {
-        return NativeUtil.newInstance(constructor, args);
+        return NativeUtil.newInstance(constructor, args == null ? new Object[0] : args);
     }
 
     @Override
